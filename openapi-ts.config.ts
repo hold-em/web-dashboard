9ca430c572c8e84357d5aac9a848d8efd5b2876e
donghun <iandonghun@gmail.com>
2025-03-05 17:37:51 +0900

    Add API

---
+++ openapi-ts.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from '@hey-api/openapi-ts';
+
+export default defineConfig({
+  input: 'https://dev.api.yajasu.kr/v3/api-docs',
+  output: 'src/lib/api',
+  plugins: ['@hey-api/client-fetch']
+});
